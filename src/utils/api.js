6f8b431f7c5fd6b40eedd787143415cee6d8b895
Hyeonjin <oboki2001@naver.com>
2025-07-27 20:56:37 +0900

    [refactor] Authorization 헤더 설정을 axios 인터셉터로 이동하여 동적 토큰 처리 가능하도록 개선

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
   headers: {
     "Content-Type": "application/json",
-    authorization: "Bearer " + sessionStorage.getItem("token"),
+    // authorization: "Bearer " + sessionStorage.getItem("token"),
   },
 });
 /**
@@ -12,11 +12,21 @@ const api = axios.create({
  */
 api.interceptors.request.use(
   (request) => {
+    // 1) 토큰이 있으면 Authorization 헤더 설정
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`;
+    } else {
+      // 2) 토큰이 없으면 헤더 제거
+      delete request.headers.Authorization;
+    }
+
     console.log("Starting Request", request);
     return request;
   },
   function (error) {
     console.log("REQUEST ERROR", error);
+    return Promise.reject(error);
   }
 );
 
