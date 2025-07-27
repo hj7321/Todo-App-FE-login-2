b9e7a6372f139569adc27d52031aefd03f397491
Hyeonjin <oboki2001@naver.com>
2025-07-28 06:45:30 +0900

    [style] Tailwind 설정에 SUIT 폰트 및 메인 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "suit-100": ["SUIT-Thin", "sans-serif"],
+        "suit-200": ["SUIT-ExtraLight", "sans-serif"],
+        "suit-300": ["SUIT-Light", "sans-serif"],
+        "suit-400": ["SUIT-Regular", "sans-serif"],
+        "suit-500": ["SUIT-Medium", "sans-serif"],
+        "suit-600": ["SUIT-SemiBold", "sans-serif"],
+        "suit-700": ["SUIT-Bold", "sans-serif"],
+        "suit-800": ["SUIT-ExtraBold", "sans-serif"],
+        "suit-900": ["SUIT-Heavy", "sans-serif"],
+      },
+      colors: {
+        main: "#a8f1ff",
+      },
+    },
   },
   plugins: [],
 };
