b1751b4235c4f86a0afff44e9be228f1349e6b42
Hyeonjin <oboki2001@naver.com>
2025-07-28 06:49:12 +0900

    [refactor] TodoBoard 컴포넌트 구조 개선 및 props 변경

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,19 +1,35 @@
-import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
+const TodoBoard = ({
+  todoList,
+  getTasks,
+  showMyTodos,
+  handleToggleMyTodos,
+  currentUser,
+}) => {
   return (
-    <div>
-      <h2>Todo List</h2>
-      {todoList.length > 0 &&
-        todoList.map((item, index) => (
-          <TodoItem
-            item={item}
-            key={index}
-            deleteItem={deleteItem}
-            toggleComplete={toggleComplete}
-          />
-        ))}
+    <div className="mt-[20px] mb-[60px] w-full">
+      <div className="flex justify-between">
+        <h2 className="font-suit-800 text-[26px]">📝 TODO LIST</h2>
+        <div></div>
+        <button onClick={handleToggleMyTodos} className="hover:font-suit-700">
+          {showMyTodos ? "모두의 TODO" : "나의 TODO"}
+        </button>
+      </div>
+      <div className="flex flex-wrap gap-[20.2px] p-[10px]">
+        {todoList.length > 0 ? (
+          todoList.map((item) => (
+            <TodoItem
+              key={item._id}
+              item={item}
+              getTasks={getTasks}
+              currentUser={currentUser}
+            />
+          ))
+        ) : (
+          <h2>There is no Item to show</h2>
+        )}
+      </div>
     </div>
   );
 };
